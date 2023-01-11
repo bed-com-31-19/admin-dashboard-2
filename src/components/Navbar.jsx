cc91517f558e11f1bd51bf6bc160ca5f2709a5ce
bed-com-31-19 <bed-com-31-19@unima.ac.mw>
2023-01-11 04:34:47 +0200

    added nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,11 +50,30 @@ const Navbar = () => {
             gap="3rem"
             p="0.1rem 1.5rem"
           >
-            <InputBase placeholder="Search..." />
+            <InputBase
+              sx={{
+                maxWidth: "7em",
+              }}
+              placeholder="Search..."
+            />
             <IconButton>
               <Search />
             </IconButton>
           </FlexBetween>
+        </FlexBetween>
+
+        {/* RIGHT SIDE */}
+        <FlexBetween gap="1.5rem">
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === "dark" ? (
+              <DarkModeOutlined sx={{ fontSize: "25px" }} />
+            ) : (
+              <LightModeOutlined sx={{ fontSize: "25px" }} />
+            )}
+                  </IconButton>
+                  <IconButton>
+                      <SettingsOutlined/>
+                  </IconButton>
         </FlexBetween>
       </Toolbar>
     </AppBar>
