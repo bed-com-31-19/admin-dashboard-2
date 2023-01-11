b5e430c9584541e6f1465b85b6743743a702c0dc
bed-com-31-19 <bed-com-31-19@unima.ac.mw>
2023-01-11 02:49:18 +0200

    complete theme setup

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
+import "./index.css";
+import { configureStore } from "@reduxjs/toolkit";
+import globalReducer from "./state/ThemeState";
+
+const store = configureStore({
+  reducer: {
+    global: globalReducer,
+  },
+});
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
